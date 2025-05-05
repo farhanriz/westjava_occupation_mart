bd7781db5ef51acb0619cceb829da34804f72385
Muhammad Farhan Rizaldi <72199468+farhanriz@users.noreply.github.com>
2025-05-05 15:04:54 +0700

    update mode .sql
    add updated_at field

--- models/mart/total_unemployed_yearly.sql
+++ models/mart/total_unemployed_yearly.sql
@@ -3,7 +3,7 @@ select
 	year,
 	occupation_group,
 	sum(number_of_people) number_of_people,
-	now()
+	now() as updated_at
 from {{ ref('total_by_occupation_group_yearly') }}
 where occupation_group = 'TIDAK/BELUM BEKERJA'
 group by 1,2
--- models/mart/total_yearly.sql
+++ models/mart/total_yearly.sql
@@ -1,7 +1,8 @@
 {{ config(materialized='view') }}
 select 
 	year,
-	sum(number_of_people) number_of_people
+	sum(number_of_people) number_of_people,
+	now() as updated_at
 from {{ ref('total_by_occupation_group_yearly') }}
 group by 1
 order by 1
